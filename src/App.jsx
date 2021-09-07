d22348aa5fa449dbbb30a5482c6852f2cc7edefb
chen <2463238851@qq.com>
2021-09-07 18:46:16 +0800

    关于表单的使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-// 在这里存放所有的入口
 import React, { Component } from 'react'
-import ThreeLayout from './components/ThreeLayout'
+// import Form from './components/Form'
+import FormTest from './components/FormTest'
 export default class App extends Component {
-    state = {
-        minWidth: 800,
-        leftWidth: 200,
-        rightWidth: 200,
-    }
     render() {
         return (
-            <ThreeLayout
-                {...this.state}
-                left={<h1>这是左边内容区域</h1>}
-                right={<h1>这是右边内容区域</h1>}
-            >
-                <div>这是中间内容区域</div>
-            </ThreeLayout>
+            <FormTest />
         )
     }
 }
